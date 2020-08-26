056fea5c1772cabf80f6a0496919066baa7106c4
czrpxr <czr-pxr@protonmail.com>
2020-08-26 03:13:28 -0300

    form1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Todo } from 'src/models/todo.model';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -9,9 +10,22 @@ import { Todo } from 'src/models/todo.model';
 export class AppComponent {
 	public todos: Todo[] = [];
     public title: String = 'Minhas Tarefas';
+    public form: FormGroup;
 
 
-	constructor() {
+	constructor(private fb: FormBuilder) {
+		this.form = this.fb.group({
+			title: ['', Validators.compose([
+				Validators.minLength(3),
+				Validators.maxLength(60),
+				Validators.required
+			])]
+
+		});
+
+
+
+
 		this.todos.push(new Todo(1, 'passear com o cachorro', false));
         this.todos.push(new Todo(2, 'ir ao supermercado', false));
         this.todos.push(new Todo(3, 'cortar o cabelo', true));
@@ -21,5 +35,22 @@ export class AppComponent {
 
 	alteraTexto() {
 		this.title = 'Teste';
+	}
+
+	remove(todo: Todo) {
+		const index = this.todos.indexOf(todo);
+
+		if(index !== -1){
+			this.todos.splice(index, 1);
+		}
+	}
+
+
+	markAsDone(todo: Todo){
+		todo.done = true;
+	}
+
+	markAsUndone(todo: Todo){
+		todo.done = false;
 	}
 }
